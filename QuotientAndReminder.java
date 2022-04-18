a04477088e4cc764c6e24620d961e2bfe6364b21
Hoang Duy <hoangduy1771@gmail.com>
2022-04-18 18:09:38 +0700

    add quotient and reminder

---
+++ QuotientAndReminder.java
@@ -0,0 +1,17 @@
+import javax.swing.SpinnerDateModel;
+
+public class QuotientAndReminder {
+    public static void main(String[] args) {
+        int num = 7;
+        int divisor = 9;
+        int quotient = 0;
+
+        while (num >= divisor) {
+            num -= divisor;
+            quotient++;
+        }
+        System.out.println(quotient);
+        System.out.println(num);
+
+    }
+}
